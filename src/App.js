82816e4f42a8b365642c49b9fbebf20bdf18a009
simonacossai <63307173+simonacossai@users.noreply.github.com>
2021-03-03 15:51:50 +0100

    DONE- deleted useless imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Main from "./Main";
-import ProfilePage from "./Components/Profile_Page/ProfilePage";
+
 function App() {
   return <Main />;
 }
